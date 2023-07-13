a20d210babea21503e89a7aeb64ece3d6d9b8d26
you-desing <youssefboulahya3@gmail.com>
2023-07-13 15:54:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
 /*
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description :a C programme with puts fonction
+ *
+ * Return : Always 0 (success)
  */
 int main(void)
 {
