d612c3daec405b392dc3956c07a7fdff56f3b7d9
you-desing <youssefboulahya3@gmail.com>
2023-07-14 07:36:34 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program for number base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int c;
+	for (c = 0; c < 10; c++)
+	{
+	printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
