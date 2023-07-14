2e2d9bc4839b603d69637ecda9b7796fea805769
you-desing <youssefboulahya3@gmail.com>
2023-07-14 07:20:52 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : A C program for alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
