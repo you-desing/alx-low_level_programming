0ac5effa030647be4fce80193d0b3a5a8e09ebd9
you-desing <youssefboulahya3@gmail.com>
2023-07-14 07:08:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program for putchar function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+	putchar(c);
+	}
+	return (0);
+}
