74a1a50744d7612d1a098cd0989023e8d2b0b0f9
you-desing <youssefboulahya3@gmail.com>
2023-07-14 08:09:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program for alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
