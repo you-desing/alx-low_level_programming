9de76f064fe1a67c000756d0fa8ac4ad12b9e33b
you-desing <youssefboulahya3@gmail.com>
2023-07-13 17:58:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  * main - Entry print
  *
  * Description : A C program with fonrtion
- *   ssize_t  write(int fd, const void *buf, sizeof(count));
+ *   ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: 1 (Error)
 */
 
 int main(void)
-{	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quote, 59);
+{	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, qu, 59);
 	return (1);
 }
