93c3908690f9affebe340b5da187cf427c732010
you-desing <youssefboulahya3@gmail.com>
2023-07-13 17:36:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program with fonrtion
+ *   ssize_t  write(int fd, const void *buf, sizeof(count));
+ *
+ * Return: 1 (Error)
+*/
+
+int main(void)
+{	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1. quote. 59);
+	return (1);
+}
