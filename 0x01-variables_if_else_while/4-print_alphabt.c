4e01c40f16c022612600243e4ddcaae040207800
you-desing <youssefboulahya3@gmail.com>
2023-07-14 07:27:30 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program for alphabet without q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if (c != 'q' && c != 'e')
+	putchar(c);
+	}
+	putchar('\n')
+	return (0);
+}
