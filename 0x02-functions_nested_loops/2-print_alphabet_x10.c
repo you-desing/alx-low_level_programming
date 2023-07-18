f1777feb38f1b70f03da9e2d1369404cf69db734
you-desing <youssefboulahya3@gmail.com>
2023-07-18 07:58:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description :the function prints 10 times the alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	print_alphabet();
+	i++;
+	}
+
+	return (0);
+}
