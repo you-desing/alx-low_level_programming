991d39b63de82147e841ea46c8cf6124e103c370
you-desing <youssefboulahya3@gmail.com>
2023-07-18 16:44:43 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,13 @@
 
 void print_to_98(int n)
 {
-int i;
+	int i;
 
 	if (i > 98)
 		for (i = n; n > 98; i--)
-			printf("%d ,", n);
+			printf("%d ,", i);
 	else
 		for (i = n; i < 98; i++)
-			printf("%d ,", n);
+			printf("%d ,", i);
 	printf("98\n");
 }
