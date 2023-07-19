1f0e64c367350202c46b735e26bc2c3fe40a34e0
you-desing <youssefboulahya3@gmail.com>
2023-07-19 08:09:39 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,13 @@
 */
 int main(void)
 {
-int n = 0, mult3, mult5;
+int n = 0;
 int s = 0;
 
 	while (n < 10)
 	{
 	if (n % 3 == 0 || n % 5 == 0)
 		{
-		mult3 = n;
-		mult5 = n;
 		s = s + n;
 		}
 	n++;
