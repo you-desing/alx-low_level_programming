4e8e6bc5ec5f030eb0ad23e19d0252616e6237e8
you-desing <youssefboulahya3@gmail.com>
2023-07-19 09:24:15 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+int n = 0;
+unsigned long s, F0 = 0, F1 = 1;
+float T;
+
+	while (1)
+	{
+	s = F0 + F1;
+	if (s > 4000000)
+		break;
+	if (s % 2 == 0)
+		T = T + s;
+	F0 = F1;
+	F1 = s;
+	n++;
+	}
+	printf("%f\n", T);
+	return (0);
+}
