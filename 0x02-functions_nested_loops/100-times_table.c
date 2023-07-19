f2f1725fea800402903a78cd180c71f53ad3c020
you-desing <youssefboulahya3@gmail.com>
2023-07-19 06:30:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,10 @@ void print_times_table(int n)
 		{
 		if (j != 0)
 			printf(",");
-		printf("%4d", i * j);
+		if (j == 0)
+			printf("%d", i * j);
+		else
+			printf("%4d", i * j);
 		}
 	printf("\n");
 	}
