febc0dbe24e696a70f31cba91472cba443d2f970
you-desing <youssefboulahya3@gmail.com>
2023-07-20 11:40:15 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,8 +14,9 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-
-	if (i % 3 == 0)
+	if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz ");
+	else if (i % 3 == 0)
 		printf("Fizz ");
 	else if (i % 5 == 0)
 		printf("Buzz ");
