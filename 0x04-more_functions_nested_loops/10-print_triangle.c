f8f6947f23b1525184b33e71b85e45cf314d0f81
you-desing <youssefboulahya3@gmail.com>
2023-07-20 12:42:42 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square -  prints a triangle
+ *
+ * @size: input the function
+ *
+*/
+
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+	for (j = 1; j <= size; j++)
+		{
+		if (i + j <= size)
+			_putchar(' ');
+		else
+			_putchar(35);
+
+		}
+	_putchar('\n');
+	}
+	}
+}
