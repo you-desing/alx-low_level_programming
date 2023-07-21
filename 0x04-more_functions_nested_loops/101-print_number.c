6586b790f443d5284f0982bfaf7579aed251ce06
you-desing <youssefboulahya3@gmail.com>
2023-07-21 05:29:52 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints all natural numbers
+ *
+ * @n: input the function
+ *
+*/
+
+void print_number(int n)
+{
+
+/* print '-' for negative numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
