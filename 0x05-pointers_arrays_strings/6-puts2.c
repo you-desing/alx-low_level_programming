447ae1382545cfe54cf72f66be38dcc918eb1872
you-desing <youssefboulahya3@gmail.com>
2023-07-24 10:26:04 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: input the function
+ *
+*/
+
+void puts2(char *str)
+{
+int i;
+	for (i = 0; i <= strlen(str); i++)
+	{
+	if (i % 2 == 0)
+		printf("%c", str[i]);
+	}
+}
