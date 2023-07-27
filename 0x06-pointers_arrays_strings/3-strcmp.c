2c2542754bb7378c8f0cc9b1db8aed373ea86300
you-desing <youssefboulahya3@gmail.com>
2023-07-27 06:28:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,18 +14,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < strlen(s1); i++)
+	while (*s1)
 	{
-		if (s1[i] == s2[i])
-
-			return (0);
-
-		else if (s1[i] > s2[i])
-
-			return (15);
-		else if (s1[i] < s2[i])
-			return (-15);
+		if (*s1 != *s2)
+		{
+			i = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+	s1++;
+	s2++;
 	}
+return (i);
 }
