1a317e1ee61fee94b038b60cd68e78c9b11d0431
you-desing <youssefboulahya3@gmail.com>
2023-07-27 07:06:00 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; i < strlen(str); i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	if (str[i] >= 'a' && str[i] <= 'z')
 		str[i] = str[i] - 32;
