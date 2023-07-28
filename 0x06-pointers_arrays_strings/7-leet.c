4ffd4f00df2e0ab3a6ccda48b4e741a27fa411b0
you-desing <youssefboulahya3@gmail.com>
2023-07-28 05:07:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,7 +3,7 @@
 /**
  * leet - Encodes a string to 1337.
  * @str: The string to be encoded.
- * done by Temesgen Abdissa
+ * 
  * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
