73a4de58729197e134c99fc4b8679c3bf146651e
you-desing <youssefboulahya3@gmail.com>
2023-08-05 10:44:13 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,13 @@
 /**
  * main - Solve me
  *
+ * @argc: argument counts
+ *
+ * @argv: argument values
+ *
  * Return: Always 0.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
 	printf("%s", *argv);
