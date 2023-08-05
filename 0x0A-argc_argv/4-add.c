c4c0b7875ee55af0d0f3a43fd046b46b145a2e06
you-desing <youssefboulahya3@gmail.com>
2023-08-05 12:33:31 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Solve me
+ *
+ * @argc: integer
+ *
+ * @argv: character
+ *
+ * Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 0)
+	{
+	printf("0\n");
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= 65 && *argv[i] <= 90 || *argv[i] >= 97 && *argv[i] <= 122)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
