b7a014c6ea2c1af4f8bfc7427bca13cdb46d0be3
you-desing <youssefboulahya3@gmail.com>
2023-08-10 09:01:36 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *a;
 
-	a = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
-	return (a);
+	else
+		return (a);
 }
