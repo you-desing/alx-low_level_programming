3fe2d23b93259611f0e710023e5d9bd3e30e88a3
you-desing <youssefboulahya3@gmail.com>
2023-08-14 11:32:35 -0700

    new-typeDog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for dog
+ */
+typedef struct dog dog_t;
 #endif
