3917259a324f1bb7afe52ea3a7e7ad5929dcc5f7
you-desing <youssefboulahya3@gmail.com>
2023-08-17 04:25:38 -0700

    simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifdef OPERA_H
+#define OPERA_H
 #include <stdio.h>
 #include <string.h>
 /**
@@ -18,3 +20,4 @@ int op_mul(int a, int b);
 int op_sub(int a, int b);
 int op_add(int a, int b);
 int (*get_op_func(char *s))(int, int);
+#endif
