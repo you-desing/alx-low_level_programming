ffeea355d46b5a141fa018930c03193ce5e4e9d4
you-desing <youssefboulahya3@gmail.com>
2023-08-17 04:22:28 -0700

    simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * @argc: first argument
+ *
+ * @argv: second argument
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char *operator;
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
+	operator = argv[2];
+	if (num2 == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	if (operator != NULL)
+	{
+	calc = (*get_op_func(operator))(num1, num2);
+	printf("%d\n", calc);
+	}
+	else
+	{
+	printf("Error\n");
+	exit(99);
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - add function
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the sum a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub function
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the sub a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multplication function
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the mult a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div function
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the div a and b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod function
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the mod a and b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
