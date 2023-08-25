19d3a50903601d395d22570eba9c6912832e4e93
you-desing <youssefboulahya3@gmail.com>
2023-08-24 17:53:43 -0700

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,9 +15,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[len])
 		len++;
+
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
