5239c69cc5f53017d61c6c6de46d90c64d0fd34a
you-desing <youssefboulahya3@gmail.com>
2023-08-29 08:05:26 -0700

    Add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp = (listint_t *)malloc(sizeof(listint_t));
 
 	if (temp != NULL)
-		return (NULLL);
+		return (NULL);
 	temp->next = NULL;
 	temp->n = n;
 	if (!*head)
