e260ae8d5e3b9a54553fa1ef3de720757f305d45
youssef <youssefboulahya3@gmail.com>
2023-09-04 20:22:01 +0100

    first task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ *
+ * @filename: name of file
+ *
+ * @letters: number of bytes
+ *
+ * Return: number byt read/printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t byt;
+	char bf[1024 * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	byt = read(fd, &bf[0], letters);
+	byt = write(STDOUT_FILENO, &bf[0], byt);
+	close(fd);
+	return (byt);
+}
