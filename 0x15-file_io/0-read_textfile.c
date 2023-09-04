a1f5057d6737b8150601cfc003fcaad06339fbab
youssef <youssefboulahya3@gmail.com>
2023-09-04 20:57:41 +0100

    aa

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,29 @@
  *
  * Return: number byt read/printed
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t byt;
-	char bf[READ_BUF_SIZE * 8];
+	ssize_t bytes_read, bytes_written;
+	char bf[1024];
 
-	if (!filename || !letters)
-		return (0);
+	if (!filename || letters == 0)
+	{
+		return (-1);
+	}
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	byt = read(fd, &bf[0], letters);
-	byt = write(STDOUT_FILENO, &bf[0], byt);
+	{
+		return (-1);
+	}
+
+	bytes_read = read(fd, bf, sizeof(bf));
+
+	bytes_written = write(STDOUT_FILENO, bf, bytes_read);
+
 	close(fd);
-	return (byt);
+
+	return (bytes_written);
 }
