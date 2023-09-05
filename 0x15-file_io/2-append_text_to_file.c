6587ac301f8b6cb145f49d7f79bcb5de6041505e
youssef <youssefboulahya3@gmail.com>
2023-09-05 11:35:47 +0100

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,30 +6,31 @@
  *
  * @text_content: the content the text
  * Return: 1 in success otherwise -1
- */
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
+	int count = 0;
+	int output = 0;
+	int fd = 0;
+
+	if (filename != NULL)
+		fd = open(filename, O_WRONLY | O_APPEND);
+	else
 		return (-1);
-	}
-	if (text_content == NULL)
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
 		return (1);
-	}
 
-	FILE *file = fopen(filename, "a");
-
-	if (file == NULL)
-	{
+	output = write(fd, text_content, count);
+	if (output == -1)
 		return (-1);
-	}
-	if (fputs(text_content, file) == EOF)
-	{
-		fclose(file);
-		return (-1);
-	}
-	fclose(file);
+	close(fd);
 	return (1);
 }
-
