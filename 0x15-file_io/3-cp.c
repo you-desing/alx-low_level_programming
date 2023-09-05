b39731fb6cec90822258e750393b05878c01aeb8
youssef <youssefboulahya3@gmail.com>
2023-09-05 15:11:21 +0100

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+
+	int fd_source = open(file_from, O_RDONLY);
+
+	if (fd_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	int fd_dest = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (fd_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd_source);
+		exit(99);
+	}
+
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
+	while ((bytes_read = read(fd_source, buffer, BUFFER_SIZE)) > 0) 
+	{
+	ssize_t bytes_written = write(fd_dest, buffer, bytes_read);
+
+	if (bytes_written == -1) 
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd_source);
+		close(fd_dest);
+		exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_source);
+		close(fd_dest);
+		exit(98);
+	}
+
+	if (close(fd_source) == -1 || close(fd_dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
+
+	exit(0);
+}
